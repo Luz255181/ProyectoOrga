582260de8e6ed8ce9981c7b0609bb0d677a3398f
Luz255181 <mira_luz_explo@hotmail.com>
2020-10-26 21:13:31 -0300

    ahora compila

--- mapeo.c
+++ mapeo.c
@@ -18,7 +18,11 @@ void crear_mapeo(tMapeo *m, int ci, int (*fHash)(void *), int (*fComparacion)(vo
     (*m)->cantidad_elementos = 0;
     (*m)->comparador = fComparacion;
     (*m)->hash_code = fHash;
-    (*m)->tabla_hash = (tLista *)malloc(longitud*sizeof(struct celda));
+    (*m)->tabla_hash = (tLista *)malloc(longitud*sizeof(tLista));
+    if((*m)->tabla_hash == NULL)
+    {
+        exit(MAP_ERROR_MEMORIA);
+    }
     for(i = 0; i<longitud;i++){
         crear_lista(((*m)->tabla_hash)+i);
     }
@@ -85,7 +89,7 @@ void m_eliminar(tMapeo m, tClave c, void (*fEliminarC)(void *), void (*fEliminar
             fEliminarV(cursor->valor);
             fEliminarC(cursor->clave);
             (m->cantidad_elementos)--;
-            l_eliminar(bucket,pos,&free());
+            l_eliminar(bucket,pos,&free);
         }
         else
         {
@@ -99,20 +103,20 @@ void m_destruir(tMapeo *m, void (*fEliminarC)(void *), void (*fEliminarV)(void *
 {
     tEntrada cursor;
     tLista bucket;
-    int i,j;
+    int i;
     tPosicion pos;
     for(i = 0; i<((*m)->longitud_tabla); i++)
     {
         bucket = *(((*m)->tabla_hash)+i);
-        while(l_longitud != 0)
+        while(l_longitud(bucket) != 0)
         {
             pos = l_primera(bucket);
             cursor = l_recuperar(bucket,pos);
             fEliminarC(cursor->clave);
             fEliminarV(cursor->valor);
-            l_eliminar(bucket,pos,&free());
+            l_eliminar(bucket,pos,&free);
         }
-        l_destruir(bucket,&free())
+        l_destruir(&bucket,&free);
     }
     free(((*m)->tabla_hash));
     free(*m);
@@ -126,7 +130,7 @@ tValor m_recuperar(tMapeo m, tClave c)
     tLista bucket;
     int hash = (m->hash_code(c))%(m->longitud_tabla);
     bucket = *((m->tabla_hash)+hash);
-    tPosicion pos = l_primera(bucket)
+    tPosicion pos = l_primera(bucket);
     for(i = 0; !encontre && i<l_longitud(bucket); i++)
     {
         cursor = (tEntrada)l_recuperar(bucket,pos);
@@ -142,4 +146,36 @@ tValor m_recuperar(tMapeo m, tClave c)
         }
     }
     return v;
+}
+
+void noEliminar(void* elem){}
+
+void reHash(tMapeo m)
+{
+    tLista * nuevo_hash;
+    tLista bucket;
+    int i, nuevaLong,hash;
+    tPosicion pos;
+    tEntrada cursor;
+    nuevaLong = (m->longitud_tabla)*2;
+    nuevo_hash = (tLista *)malloc(nuevaLong*sizeof(tLista));
+    for(i = 0; i<nuevaLong;i++)
+    {
+        crear_lista(nuevo_hash+i);
+    }
+    for(i = 0; i<(m->longitud_tabla);i++)
+    {
+        bucket = *((m->tabla_hash)+i);
+        while((l_longitud(bucket))!=0)
+        {
+            pos = l_primera(bucket);
+            cursor = l_recuperar(bucket,pos);
+            hash = (m->hash_code(cursor->clave))%(nuevaLong);
+            l_insertar((*(nuevo_hash+hash)),l_primera(*(nuevo_hash+hash)),cursor);
+            l_eliminar(bucket,pos,&noEliminar);
+        }
+        l_destruir(&bucket,&noEliminar);
+    }
+    free(m->tabla_hash);
+    m->tabla_hash=nuevo_hash;
 }
