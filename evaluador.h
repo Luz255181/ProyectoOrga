4286956b24d21c0b61d7dc40e99bd7fe600a1706
unknown <valentin.pisani@hotmail.com>
2020-10-28 12:17:25 -0300

    agregue el evaluador

---
+++ evaluador.h
@@ -0,0 +1,6 @@
+#ifndef EVALUADOR_H_INCLUDED
+#define EVALUADOR_H_INCLUDED
+
+extern void contador(tMapeo map,int (*fHash)(void *), int (*fComparacion)(void *, void *));
+
+#endif // EVALUADOR_H_INCLUDED
