141085ef67c5b598a5d6fc823711de38defba415
nahueld97 <nahuedl97@outlook.com>
2020-10-29 00:12:35 -0300

    bore archivos

--- evaluador.h
+++
@@ -1,6 +0,0 @@
-#ifndef EVALUADOR_H_INCLUDED
-#define EVALUADOR_H_INCLUDED
-
-extern void contador(tMapeo map,int (*fHash)(void *), int (*fComparacion)(void *, void *));
-
-#endif // EVALUADOR_H_INCLUDED
